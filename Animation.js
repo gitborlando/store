bd8292ef92f1f3763553813eb1b4d5093401260f
皮皮歪 <72959369+gitborlando@users.noreply.github.com>
2021-03-16 12:43:12 +0800

    对上一版本的Animation.js做了一个小幅度升级，可以设置多个属性同时进行动画，并且可以为每个属性设置不同的曲线函数

--- Animation.js
+++ Animation.js
@@ -2,82 +2,103 @@ HTMLElement.prototype.FrameAnimation = function (option) {
     let start = 0
     let movingCurve = 0
     let percent = 0
-    let origin = 0
     let _this = this
-    let prohibition = -1
-    let haveStart = -1
     let state = -1
-    let range = Number(option.range.match(/\d+/)[0])
-    let unit = option.range.match(/[^\d]+/)[0]
-    this.startAnimation = function (args) {
-        if (args.delay) return Delay(args.delay, args.timing, 2, args.callback)
-        return render.bind(null, args.timing, 2, args.callback)
+    let attrList = []
+    let rangeList = []
+    let _rangeList = []
+    let timingList = []
+    let originAttrList = []
+    let _originAttrList = []
+    let animations = option.animation
+    if (animations.constructor == {}.constructor) animations = [animations]
+    animations.forEach((each) => {
+        rangeList = [...rangeList, ...Object.values(each).filter((i) => { return !(i instanceof Function) })]
+        attrList = [...attrList, ...Object.keys(each).filter((i) => { return rangeList.includes(each[i]) })]
+        timingList = [...timingList, ...Object.values(each).filter((i) => { return i instanceof Function })]
+    })
+    let newList = []
+    rangeList.forEach((each) => {
+        each = grtRangeAndUnit(each)
+        newList.push(each)
+    })
+    rangeList = _rangeList = newList
+    originAttrList = getOriginAttrList()
+    this.startAnimation = function (...args) {
+        return advanceOrBack(2, args)
     }
-    this.turnBack = function (args) {
-        if (args.delay) return Delay(args.delay, args.timing, 1, args.callback)
-        return render.bind(null, args.timing, 1, args.callback)
+    this.turnBack = function (...args) {
+        return advanceOrBack(1, args)
     }
-    let Delay = function (delay, timing, direction) {
+    function advanceOrBack(direction, args) {
+        if (args.length == 0) return render.bind(null, direction)
+        if (args.length == 1 && typeof args[0] == 'number') return Delay(args[0], direction)
+        if (args.length == 1 && args[0] instanceof Function) return render.bind(null, direction, args[0])
+        return Delay(args[1], direction, args[0])
+    }
+    function Delay(delay, direction, callback) {
         return function () {
             setTimeout(() => {
-                render.call(null, timing, direction)
+                render(direction, callback)
             }, delay)
         }
     }
-    let render = function (timing, direction, callback) {
-        if (prohibition ** (direction) == state) return
-        if (!(haveStart ** direction == 1)) return
+    function render(direction, callback) {
+        if ((-1) ** (direction) == state) return
         state = (-1) ** direction
-        haveStart = (-1) ** direction
         start = performance.now()
-        origin = parseInt(getComputedStyle(_this).getPropertyValue(option.attribute))
-        if (unit == '%') origin = parseInt(getComputedStyle(_this)
-            .getPropertyValue(option.attribute)) / option.parentsRange
+        rangeList = direction == 1 ? rangeList.map((i) => { return [i[0] * (-1), i[1]] }) : _rangeList
+        originAttrList = _originAttrList.length == 0 ? originAttrList : _originAttrList
         requestAnimationFrame(function animate(NowTime) {
-            percent = (NowTime - start) / option.duration
-            percent = percent > 1 ? 1 : percent
-            movingCurve = timing ? timing(percent) : percent
-            _this.style[option.attribute] = origin + range * movingCurve * (-1) ** direction + unit
-            if (percent < 1) { requestAnimationFrame(animate) }
-            _this.animationPercent = percent
-            if (callback) callback(percent)
+            attrList.forEach((each, index) => {
+                let timing = timingList[index]
+                let rangeAndUnit = rangeList[index]
+                percent = (NowTime - start) / option.duration
+                percent = percent > 1 ? 1 : percent
+                movingCurve = timing ? timing(percent) : percent
+                _this.style[each] = originAttrList[index] + rangeAndUnit[0] * movingCurve + rangeAndUnit[1]
+            })
+            if (percent < 1) { requestAnimationFrame(animate) } else {
+                _originAttrList = getOriginAttrList()
+            }
+            if (callback instanceof Function) callback(percent)
         })
+    }
+    function getOriginAttrList() {
+        let aList = []
+        attrList.forEach((each) => {
+            let originAttr = parseFloat(getComputedStyle(_this).getPropertyValue(each))
+            aList.push(originAttr)
+        })
+        return aList
+    }
+    function grtRangeAndUnit(each) {
+        let unit = (typeof each == 'string' && each.match(/[^-?\d*\.?\d*]+/)) ? each.match(/[^-?\d*\.?\d*]+/)[0] : 0
+        let range = parseFloat(each)
+        return [range, unit]
     }
     return this
 }
 
-let div1 = document.querySelector('.app')
-let div2 = document.querySelector('.app2')
-let div3 = document.querySelector('.father')
+/*Useage*/
 
+let div1 = document.querySelector('.div1')
+let div2 = document.querySelector('.div2')
 
-div3.addEventListener('mouseenter', (e) => {
-    if (e.target.className.match(/app/)) {
-        e.target.FrameAnimation({
-            attribute: 'width',
-            range: '300px',
-            duration: 1000,
-        }).startAnimation({
-            timing: (percent) => {
-                return percent ** (1 - percent)
-            },
-            callback: (p) => {
-                if (p == 1) {
-                    e.target.turnBack({
-                        timing: (percent) => {
-                            return percent ** (1 - percent)
-                        },
-                        delay: 1500
-                    })()
-                }
-            }
-        })()
+div1.onclick = div1.FrameAnimation({
+    animation: [
+        { 'left': '-100px' },
+        { 'opacity': '0.8', },
+    ],
+    duration: 500
+}).startAnimation((p) => {
+    if (p >= 1) { 
+        div2.startAnimation(3000)()
     }
-}, true)
-
-
-
-
-
-
-
+})
+div2.FrameAnimation({
+    animation: [
+        { 'left': '100px' }
+    ],
+    duration: 500
+})
