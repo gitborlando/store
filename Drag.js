6e1872757cd790ee8d0f7c64f448df4f67b9fa5e
皮皮歪 <72959369+gitborlando@users.noreply.github.com>
2021-03-17 11:51:34 +0800

    解决了鼠标拖动太快导致元素跟不上的问题

--- Drag.js
+++ Drag.js
@@ -1,22 +1,23 @@
 HTMLElement.prototype.drag = function (mousePosition) {
     let positionShift = []
+    let onMouseMove = function (event) {
+        this.setThisPosition(event.pageX, event.pageY)
+    }.bind(this)
     this.setThisPosition = function (pageX, pageY) {
         this.style.left = pageX - positionShift[0] + 'px'
         this.style.top = pageY - positionShift[1] + 'px'
     }
-    this.onMouseMove = function (event) {
-        this.setThisPosition(event.pageX, event.pageY)
-    }
     this.onMouseUp = function () {
-        this.removeEventListener('mousemove', this.onMouseMove)
+        document.removeEventListener('mousemove', onMouseMove)
+        document.removeEventListener('mouseup', () => { this.onMouseUp() })
     }
     this.ondragstart = function () { return false }
-    this.addEventListener('mouseup', this.onMouseUp)
     this.style.position = 'absolute'
     this.style.zIndex = 1000
     document.body.append(this)
     positionShift[0] = mousePosition.clientX - this.getBoundingClientRect().left
     positionShift[1] = mousePosition.clientY - this.getBoundingClientRect().top
     this.setThisPosition(mousePosition.pageX, mousePosition.pageY)
-    this.addEventListener('mousemove', this.onMouseMove)
+    document.addEventListener('mousemove', onMouseMove)
+    document.addEventListener('mouseup', () => { this.onMouseUp() })
 }
