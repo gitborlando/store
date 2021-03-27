b30cd13468d268f942a86ab001abdbb420c983aa
皮皮歪 <72959369+gitborlando@users.noreply.github.com>
2021-03-27 22:17:09 +0800

    添加了this.Destroy()钩子函数用于在确定将要销毁动画组件后,释放组件内未被释放的变量(因为使用了闭包).FrameRippleEffect效果使用getBoundingRect()解决了padding情况下动画偏移的Bug

--- Animation.js
+++ Animation.js
@@ -1,30 +1,30 @@
 HTMLElement.prototype.FrameAnimation = function (option) {
-    this.start = 0 
-    this.movingCurve = 0 
-    this.percent = 0 
-    this.state = -1 
-    this.prohibition = 0 
-    this.stopAnimation = 0 
-    this.attrList = [] 
-    this.rangeList = [] 
-    this._rangeList = [] 
-    this.timingList = [] 
-    this.originAttrList = []
-    this._originAttrList = []
-    this.animations = option.animation
-    this.duration = option.duration
-    this.freezen = option.freezen ?? true
-    if (this.animations.constructor == {}.constructor) this.animations = [this.animations]
-    this.animations.forEach((each) => {
-        this.rangeList = [...this.rangeList, ...Object.values(each).filter((i) => { return !(i instanceof Function) })]
-        this.attrList = [...this.attrList, ...Object.keys(each).filter((i) => { return this.rangeList.includes(each[i]) })]
-        this.eachTiming = Object.values(each).filter((i) => { return i instanceof Function }).length !== 0 ?
+    let start = 0
+    let movingCurve = 0
+    let percent = 0
+    let state = -1
+    let prohibition = 0
+    let stopAnimation = 0
+    let attrList = []
+    let rangeList = []
+    let _rangeList = []
+    let timingList = []
+    let originAttrList = []
+    let _originAttrList = []
+    let animations = option.animation
+    let duration = option.duration
+    let freezen = option.freezen === undefined ? true : option.freezen
+    if (animations.constructor == {}.constructor) animations = [animations]
+    animations.forEach((each) => {
+        rangeList = [...rangeList, ...Object.values(each).filter((i) => { return !(i instanceof Function) })]
+        attrList = [...attrList, ...Object.keys(each).filter((i) => { return rangeList.includes(each[i]) })]
+        eachTiming = Object.values(each).filter((i) => { return i instanceof Function }).length !== 0 ?
             Object.values(each).filter((i) => { return i instanceof Function }) : [undefined]
-        this.timingList = [...this.timingList, ...this.eachTiming]
+        timingList = [...timingList, ...eachTiming]
     })
-    this.getOriginAttrList = function () {
+    let Frame_getOriginAttrList = () => {
         let aList = []
-        this.attrList.forEach((each) => {
+        attrList.forEach((each) => {
             let _each = ''
             if (['rotate', 'skew', 'scale', 'translate'].includes(each)) {
                 _each = each; each = 'transform'
@@ -38,105 +38,134 @@ HTMLElement.prototype.FrameAnimation = function (option) {
         })
         return aList
     }
-    this.sinCosToAngle = function (matrix) {
+    let Frame_sinCosToAngle = function (matrix) {
         let cos = parseFloat(matrix.match(/(?<=\().+(?=\))/)[0].split(',')[0])
         return parseFloat((Math.acos(cos) / Math.PI * 180).toFixed(2))
     }
-    this.angleToSinCos = function (angle) {
+    let Frame_angleToSinCos = function (angle) {
         let sin = Math.sin(angle / 180 * Math.PI).toFixed(6)
         let cos = Math.cos(angle / 180 * Math.PI).toFixed(6)
         return [parseFloat(sin), parseFloat(cos)]
     }
-    this.matrixToScale = function (matrix) {
+    let Frame_matrixToScale = function (matrix) {
         let scale = parseFloat(matrix.match(/(?<=\().+(?=\))/)[0].split(',')[0])
         return scale
     }
-    this.getRangeAndUnit = function (each) {
+    let Frame_getRangeAndUnit = function (each) {
         let unit = (typeof each == 'string' && each.match(/[^-?\d*\.?\d*]+/)) ?
             each.match(/[^-?\d*\.?\d*]+/)[0] : 0
         let range = parseFloat(each)
         return [range, unit]
     }
     let newList = []
-    this.rangeList.forEach((each) => {
-        each = this.getRangeAndUnit(each)
+    rangeList.forEach((each) => {
+        each = Frame_getRangeAndUnit(each)
         newList.push(each)
     })
-    this.rangeList = this._rangeList = newList
-    this.originAttrList = this.getOriginAttrList()
-    this.render = function (direction, callback) {
-        if (this.prohibition) return
-        this.prohibition = 1
-        if (this.freezen) {
-            if ((-1) ** direction == this.state) return
-            this.state = (-1) ** direction
+    rangeList = _rangeList = newList
+    originAttrList = Frame_getOriginAttrList()
+    let Frame_render = (direction, callback) => {
+        if (prohibition) return
+        prohibition = 1
+        if (freezen) {
+            if ((-1) ** direction == state) return
+            state = (-1) ** direction
         }
-        this.start = performance.now()
-        this.rangeList = direction == 1 ? this.rangeList.map((i) => { return [i[0] * (-1), i[1]] }) : this._rangeList
-        this.originAttrList = this._originAttrList.length == 0 ? this.originAttrList : this._originAttrList
+        start = performance.now()
+        rangeList = direction == 1 ? rangeList.map((i) => { return [i[0] * (-1), i[1]] }) : _rangeList
+        originAttrList = _originAttrList.length == 0 ? originAttrList : _originAttrList
         requestAnimationFrame(function animate(NowTime) {
-            this.attrList.forEach((each, index) => {
-                let timing = this.timingList[index]
-                this.rangeAndUnit = this.rangeList[index]
-                this.percent = (NowTime - this.start) / this.duration
-                this.percent = this.percent > 1 ? 1 : this.percent
-                this.movingCurve = timing ? timing(this.percent) : this.percent
-                if (typeof this.originAttrList[index] == 'string' && this.originAttrList[index].match(/matrix/)) {
-                    if (this.originAttrList[index].match(/rotate/)) {
-                        let angle = this.sinCosToAngle(this.originAttrList[index])
-                            + this.rangeAndUnit[0] * this.movingCurve + this.rangeAndUnit[1]
-                        let sin = this.angleToSinCos(angle)[0]
-                        let cos = this.angleToSinCos(angle)[1]
-                        this.style.transform = `matrix(${cos}, ${sin}, ${-1 * sin}, ${cos}, 0, 0)`
-                    } else if (this.originAttrList[index].match(/scale/)) {
-                        let originScale = this.matrixToScale(this.originAttrList[index])
-                        let scale = originScale + (this.rangeAndUnit[0] - 1) * this.movingCurve + this.rangeAndUnit[1]
+            attrList.forEach((each, index) => {
+                let timing = timingList[index]
+                rangeAndUnit = rangeList[index]
+                percent = (NowTime - start) / duration
+                percent = percent > 1 ? 1 : percent
+                movingCurve = timing ? timing(percent) : percent
+                if (typeof originAttrList[index] == 'string' && originAttrList[index].match(/matrix/)) {
+                    if (originAttrList[index].match(/rotate/)) {
+                        let angle = Frame_sinCosToAngle(originAttrList[index])
+                            + rangeAndUnit[0] * movingCurve + rangeAndUnit[1]
+                        let sin = Frame_angleToSinCos(angle)[0]
+                        let cos = Frame_angleToSinCos(angle)[1]
+                        style.transform = `matrix(${cos}, ${sin}, ${-1 * sin}, ${cos}, 0, 0)`
+                    } else if (originAttrList[index].match(/scale/)) {
+                        let originScale = Frame_matrixToScale(originAttrList[index])
+                        if (direction == 1) {
+                            var scale = originScale + (rangeAndUnit[0] + 1) * movingCurve + rangeAndUnit[1]
+                        } else {
+                            var scale = originScale + (rangeAndUnit[0] - 1) * movingCurve + rangeAndUnit[1]
+                        }
                         this.style.transform = `matrix(${scale}, 0, 0, ${scale}, 0, 0)`
                     }
                 }
                 else {
-                    this.style[each] = this.originAttrList[index] + this.rangeAndUnit[0] * this.movingCurve + this.rangeAndUnit[1]
+                    this.style[each] = originAttrList[index] + rangeAndUnit[0] * movingCurve + rangeAndUnit[1]
                 }
             })
-            if (this.percent < 1) {
-                if (this.stopAnimation) return
+            if (percent < 1) {
+                if (stopAnimation) return
                 requestAnimationFrame(animate.bind(this))
             } else {
-                this.prohibition = 0
-                this._originAttrList = this.getOriginAttrList()
+                prohibition = 0
+                _originAttrList = Frame_getOriginAttrList()
             }
-            if (callback instanceof Function) callback(this.percent)
+            if (callback instanceof Function) callback(percent)
         }.bind(this))
     }
-    this.startAnimation = function (...args) {
-        return this.delayOrCallback(2, args)
+    let Frame_Delay = function (delay, direction, callback) {
+        return function () {
+            setTimeout(() => {
+                Frame_render(direction, callback)
+            }, delay)
+        }.bind(this)
     }
-    this.turnBack = function (...args) {
-        this.freezen = false
-        return this.delayOrCallback(1, args)
+    let Frame_delayOrCallback = function (direction, args) {
+        if (args.length == 0) return Frame_render.bind(this, direction)
+        if (args.length == 1 && typeof args[0] == 'number') return Frame_Delay(args[0], direction)
+        if (args.length == 1 && args[0] instanceof Function) return Frame_render.bind(this, direction, args[0])
+        return Frame_Delay(args[1], direction, args[0])
     }
-    this.loop = function (percent, backDelay, startDelay) {
-        if (percent >= 1) this.turnBack((percent) => {
-            if (percent >= 1) this.startAnimation((percent) => {
-                this.loop(percent, backDelay, startDelay)
+    this.StartAnimation = function (...args) {
+        return Frame_delayOrCallback(2, args)
+    }
+    this.TurnBack = function (...args) {
+        freezen = false
+        return Frame_delayOrCallback(1, args)
+    }
+    this.Loop = function (percent, backDelay, startDelay) {
+        if (percent >= 1) turnBack((percent) => {
+            if (percent >= 1) startAnimation((percent) => {
+                loop(percent, backDelay, startDelay)
             }, startDelay)()
         }, backDelay)()
     }
-    this.stop = function () {
-        return () => { this.stopAnimation = 1 }
+    this.Stop = function () {
+        return () => { stopAnimation = 1 }
     }
-    this.delayOrCallback = function (direction, args) {
-        if (args.length == 0) return this.render.bind(this, direction)
-        if (args.length == 1 && typeof args[0] == 'number') return this.Delay(args[0], direction)
-        if (args.length == 1 && args[0] instanceof Function) return this.render.bind(this, direction, args[0])
-        return this.Delay(args[1], direction, args[0])
-    }
-    this.Delay = function (delay, direction, callback) {
-        return function () {
-            setTimeout(() => {
-                this.render(direction, callback)
-            }, delay)
-        }.bind(this)
+    this.Destroy = function () {
+        start = 0
+        movingCurve = 0
+        percent = 0
+        state = -1
+        prohibition = 0
+        stopAnimation = 0
+        attrList = []
+        rangeList = []
+        _rangeList = []
+        timingList = []
+        originAttrList = []
+        _originAttrList = []
+        animations = []
+        duration = 0
+        freezen = false
+        Frame_getOriginAttrList = null
+        Frame_sinCosToAngle = null
+        Frame_angleToSinCos = null
+        Frame_matrixToScale = null
+        Frame_getRangeAndUnit = null
+        Frame_render = null
+        Frame_Delay = null
+        Frame_delayOrCallback = null
     }
     return this
 }
@@ -151,38 +180,39 @@ HTMLElement.prototype.FrameAnimation = function (option) {
 //     ],
 //     duration: 2500,//时间
 //     freezen: true,// 这个属性意思是动画进行了一次后就不能再进行了{限制只运行一次},使用turnBack后可解锁
-// }).startAnimation((p) => { //startAnimation接受一个回调函数和一个延迟时间{延迟x秒进行动画}作为参数,这个p仍然是完成百分比{0~1之间}
-//     d1.loop(p, 1000, 1000) //loop作用是使动画循环播放,参数{p,1000,1000}=>{完成度,延迟1s原路返回,返回后再延迟1s重新开始动画,并循环调用}
-//     if (p >= 1) d1.turnBack()() //if (p >= 1) turnBack()的作用是在动画结束后再原路返回,也接受一个回调函数和一个延迟时间
+// }).StartAnimation((p) => { //startAnimation接受一个回调函数和一个延迟时间{延迟x秒进行动画}作为参数,这个p仍然是完成百分比{0~1之间}
+//     d1.Loop(p, 1000, 1000) //loop作用是使动画循环播放,参数{p,1000,1000}=>{完成度,延迟1s原路返回,返回后再延迟1s重新开始动画,并循环调用}
+//     if (p >= 1) d1.TurnBack()() //if (p >= 1) TurnBack()的作用是在动画结束后再原路返回,也接受一个回调函数和一个延迟时间
 // }, 3000) //3000=>延迟时间,可以单写一个函数,也可以单写一个时间,两个都写当然也可以
-// button.onclick = d1.stop() //动画停止
+// button.onclick = d1.Stop() //动画停止
+// button.Destroy() //如果确定了以后不需要这个动画后九可以调用这个函数把不必要的变量清除,以防性能下降
 
 HTMLElement.prototype.FrameRippleEffect = function (option) {
-    this.color = option.color
-    this.range = option.range
-    this.duration = option.duration
-    this.timing = option.timing
-    this.render = function (e) {
+    let color = option.color
+    let range = option.range
+    let duration = option.duration
+    let timing = option.timing
+    let Frame_render = (e) => {
         let cir = document.createElement('div')
-        cir.style['background-color'] = this.color
+        cir.style['background-color'] = color
         cir.style['border-radius'] = '50%'
         cir.style['position'] = 'absolute'
         cir.style['width'] = '1px'
         cir.style['height'] = '1px'
-        cir.style['left'] = e.clientX - parseFloat(getComputedStyle(this).getPropertyValue('left')) + 'px'
-        cir.style['top'] = e.clientY - parseFloat(getComputedStyle(this).getPropertyValue('top')) + 'px'
+        cir.style['left'] = e.clientX - this.getBoundingClientRect().left + 'px'
+        cir.style['top'] = e.clientY - this.getBoundingClientRect().top + 'px'
         this.appendChild(cir)
         cir.FrameAnimation({
             animation: [
-                { 'scale': this.range, timing: this.timing },
-                { 'opacity': '-1', timing: this.timing }
+                { 'scale': range, timing },
+                { 'opacity': '-1', timing }
             ],
-            duration: this.duration
-        }).startAnimation((p)=>{ if(p>=1) this.removeChild(cir) })()
+            duration,
+        }).StartAnimation((p) => { if (p >= 1) this.removeChild(cir) })()
     }
-    this.addEventListener('mousedown', this.render)
-    this.addEventListener('mouseup', ()=>{
-        removeEventListener('mousedown', this.render)
+    this.addEventListener('mousedown', Frame_render)
+    this.addEventListener('mouseup', () => {
+        removeEventListener('mousedown', Frame_render)
     })
     this.style.overflow = 'hidden'
     return this
