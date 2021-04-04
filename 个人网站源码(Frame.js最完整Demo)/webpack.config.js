23d3ebea234dd4e1acf717c839d1ea7e285fa342
皮皮歪 <72959369+gitborlando@users.noreply.github.com>
2021-04-04 20:35:34 +0800

    Update webpack.config.js

--- 个人网站源码(Frame.js最完整Demo)/webpack.config.js
+++ 个人网站源码(Frame.js最完整Demo)/webpack.config.js
@@ -1,9 +1,24 @@
-const path = require('path');
-
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'boundle.js',
     path: __dirname
-  }
-};
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,    //打包规则应用到以css结尾的文件上
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|svg|eot|svg+xml)$/,
+        use: 'url-loader'
+      },
+      {
+        test: /\.(jpg|jpeg|webp|png|gif)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  watch: true,
+}
